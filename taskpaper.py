585d99c6f024d90d826df9182d30d6bbb098c310
Alex Chan <alex@alexwlchan.net>
2016-04-03 10:03:24 +0100

    Basic TaskPaperDocument class

---
+++ taskpaper.py
@@ -0,0 +1,29 @@
+import tempfile
+
+
+class TaskPaperError(Exception):
+    pass
+
+
+class TaskPaperDocument(object):
+
+    def __init__(self, path):
+        self.path = path
+        self.text = self._read()
+
+    def _read(self):
+        try:
+            with open(self.path) as infile:
+                return infile.read()
+        except IOError as e:
+            raise TaskPaperError("Unable to read document %s: %s" % (path, e))
+
+    def write(self):
+        """
+        Write out the list of tasks.  Writes are atomic.
+        """
+        _, tmp_path = tempfile.mkstemp(prefix='taskpaper')
+        with open(tmp_path, 'w') as outfile:
+            outfile.write(self.text)
+
+        shutil.move(tmp_path, self.path)
