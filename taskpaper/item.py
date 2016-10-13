2263acd670ef4d4e9e202e965744dbcceb058e12
Alex Chan <alex@alexwlchan.net>
2016-10-13 08:46:48 +0100

    Add a basic tree structure with parent-child relationships

--- taskpaper/item.py
+++ taskpaper/item.py
@@ -3,8 +3,35 @@
 
 class TaskPaperItem(object):
 
-    def __init__(self, tab_size=4):
+    def __init__(self, text, tab_size=4, parent=None):
         """
         :param tab_size: Number of spaces used per indentation level.
+        :param parent: Parent task.
         """
         self.tab_size = tab_size
+        self.children = []
+        self.parent = parent
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, new_parent):
+        # If we've never had a parent, default to None before proceeding
+        if not hasattr(self, '_parent'):
+            self._parent = None
+
+        # If this is the same as our old parent, this is a no-op
+        if self._parent is new_parent:
+            return
+
+        # If we already had a parent, remove ourselves from its children
+        if self._parent is not None:
+            self._parent.children.remove(self)
+
+        # If the new parent is not None, add ourselves to its children
+        if new_parent is not None:
+            new_parent.children.append(self)
+
+        self._parent = new_parent
--- test/test_item.py
+++ test/test_item.py
@@ -10,3 +10,58 @@ from taskpaper import TaskPaperItem
 def test_setting_tab_size(tab_size):
     item = TaskPaperItem(tab_size=tab_size)
     assert item.tab_size == tab_size
+
+
+class TestParentChildRelationship(object):
+
+    def test_default_parent_is_none(self):
+        """By default, a task does not have a parent."""
+        item = TaskPaperItem('hello world')
+        assert item.parent is None
+
+    def test_default_task_has_no_children(self):
+        """By default, a task has no children."""
+        item = TaskPaperItem('hello world')
+        assert item.children == []
+
+    def test_setting_a_parent(self):
+        """Test we can initialize an item with a parent."""
+        item_p = TaskPaperItem('parent')
+        item_c = TaskPaperItem('child', parent=item_p)
+        assert item_c.parent == item_p
+        assert item_p.children == [item_c]
+
+    def test_updating_a_parent(self):
+        """Test we can create an item with a parent, then change the parent."""
+        item_p1 = TaskPaperItem('parent1')
+        item_p2 = TaskPaperItem('parent2')
+        item_c = TaskPaperItem('child', parent=item_p1)
+
+        item_c.parent = item_p2
+        assert item_c.parent == item_p2
+        assert item_p2.children == [item_c]
+        assert item_p1.children == []
+
+    def test_updating_to_same_parent(self):
+        """
+        Create an item with a parent, change the parent to existing parent,
+        check nothing happens.
+        """
+        item_p = TaskPaperItem('parent')
+        item_c = TaskPaperItem('child', parent=item_p)
+
+        item_c.parent == item_p
+        assert item_c.parent == item_p
+        assert item_p.children == [item_c]
+
+    def test_removing_a_parent(self):
+        """
+        Create an item with a parent, then set the parent to None.  Check the
+        child is removed from the list of its previous parents' children.
+        """
+        item_p = TaskPaperItem('parent')
+        item_c = TaskPaperItem('child', parent=item_p)
+
+        item_c.parent = None
+        assert item_c.parent is None
+        assert item_p.children == []
